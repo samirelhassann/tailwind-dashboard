b1c15be6dd5a8fed1c690884bc88bfbbe9e8e2d9
Samir El Hassan <samirelhassann@gmail.com>
2023-08-22 08:31:30 -0300

    Commit

---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Logo } from "@/app/assets/Logo";
+
+export function Sidebar() {
+  return (
+    <aside className="border-r border-zinc-200 px-10 py-8">
+      <Logo />
+
+      <div className="" />
+    </aside>
+  );
+}
